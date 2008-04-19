7396246ff81e85a479b39bd3a7a465ad79b11e15
hiroki@loki <hiroki@loki>
2008-04-20 00:43:16 +0900

    reader: add function to read from string

--- reader.c
+++ reader.c
@@ -8,16 +8,112 @@
 
 #define BUFFER_SIZE 128
 
+typedef int (*ReadLineFunc)(ScmReader *reader, char *buffer, size_t size);
+typedef int (*ReadCharFunc)(ScmReader *reader);
+typedef bool (*IsEOFFunc)(ScmReader *reader);
+
 struct ScmReaderRec {
-  FILE *input;
   unsigned char *buffer;
   size_t size;
   size_t head;
   size_t used;
   int line;
   int column;
+  ReadLineFunc readline;
+  ReadCharFunc readchar;
+  IsEOFFunc    is_eof;
 };
 
+typedef struct {
+  ScmReader base;
+  FILE *input;
+} ScmFileReader;
+
+typedef struct {
+  ScmReader base;
+  const char *string;
+  size_t len;
+  const char *ptr;
+} ScmStringReader;
+
+
+static int
+read_line_from_file(ScmReader *reader, char *buffer, size_t size)
+{
+  ScmFileReader *file_reader = (ScmFileReader *)reader;
+  char *p;
+
+  assert(file_reader != NULL);
+
+  p = fgets(buffer, size, file_reader->input);
+  if (p == NULL)
+    return 0;
+  else
+    return strlen(p);
+}
+
+static int
+read_char_from_file(ScmReader *reader)
+{
+  ScmFileReader *file_reader = (ScmFileReader *)reader;
+  assert(file_reader != NULL);
+  return fgetc(file_reader->input);
+}
+
+static bool
+is_file_eof(ScmReader *reader)
+{
+  ScmFileReader *file_reader = (ScmFileReader *)reader;
+  assert(file_reader != NULL);
+  return (feof(file_reader->input) ? true : false);
+}
+
+static int
+read_line_from_string(ScmReader *reader, char *buffer, size_t size)
+{
+  ScmStringReader *str_reader = (ScmStringReader *)reader;
+  const char *tail;
+  size_t readlen;
+
+  assert(str_reader != NULL);
+
+  tail = strchr(str_reader->ptr, '\n');
+  if (tail == NULL)
+    tail = str_reader->string + str_reader->len;
+  else
+    tail++;
+
+  readlen = str_reader->ptr - tail;
+  if (readlen >= size)
+    readlen = size - 1;
+
+  strncpy(buffer, str_reader->ptr, readlen);
+  buffer[readlen] = '\0';
+  str_reader->ptr += readlen;
+
+  return readlen;
+}
+
+static int
+read_char_from_string(ScmReader *reader)
+{
+  ScmStringReader *str_reader = (ScmStringReader *)reader;
+
+  assert(str_reader != NULL);
+
+  if (str_reader->string - str_reader->ptr >= str_reader->len)
+    return EOF;
+
+  return (int)(*str_reader->ptr++);
+}
+
+static bool
+is_string_eof(ScmReader *reader)
+{
+  ScmStringReader *str_reader = (ScmStringReader *)reader;
+  assert(str_reader != NULL);
+  return (str_reader->string - str_reader->ptr >= str_reader->len) ? true : false;
+}
 
 static void
 scm_reader_normalize_buffer(ScmReader *reader, size_t needed_size)
@@ -63,7 +159,7 @@ scm_reader_chuck_off_char(ScmReader* reader)
 
   if (reader->head > reader->used) {
     size_t chuck_off = reader->head - reader->used;
-    while (chuck_off-- > 0) fgetc(reader->input);
+    while (chuck_off-- > 0) reader->readchar(reader);
   }
 }
 
@@ -75,12 +171,12 @@ scm_reader_access(ScmReader *reader, size_t index)
   assert(reader != NULL);
 
   if (reader->head + index >= reader->used) {
-    if (feof(reader->input)) return EOF;
+    if (reader->is_eof(reader)) return EOF;
 
     scm_reader_chuck_off_char(reader);
     scm_reader_normalize_buffer(reader, index + 1);
-    fgets((char *)reader->buffer + reader->used,
-          reader->size - reader->used, reader->input);
+    reader->readline(reader, (char *)reader->buffer + reader->used,
+                     reader->size - reader->used);
     reader->used = strlen((char *)reader->buffer);
   }
 
@@ -91,21 +187,56 @@ scm_reader_access(ScmReader *reader, size_t index)
     return reader->buffer[offset];
 }
 
-ScmReader *
-scm_reader_construct(FILE *input)
+static void
+scm_initialize_base(ScmReader *reader,
+                    ReadLineFunc readline,
+                    ReadCharFunc readchar,
+                    IsEOFFunc is_eof)
 {
-  ScmReader *reader;
+  assert(reader != NULL);
 
-  assert(input != NULL);
-
-  reader = scm_memory_allocate(sizeof(ScmReader));
-  reader->input = input;
   reader->buffer = scm_memory_allocate(BUFFER_SIZE);
   reader->size = BUFFER_SIZE;
   reader->buffer[0] = '\0';
   reader->head = 0;
   reader->used = 0;
-  return reader;
+  reader->readline = readline;
+  reader->readchar = readchar;
+  reader->is_eof = is_eof;
+}
+
+ScmReader *
+scm_reader_construct(FILE *input)
+{
+  ScmFileReader *reader;
+
+  assert(input != NULL);
+
+  reader = scm_memory_allocate(sizeof(ScmFileReader));
+  scm_initialize_base(&reader->base,
+                      read_line_from_file,
+                      read_char_from_file,
+                      is_file_eof);
+  reader->input = input;
+  return (ScmReader *)reader;
+}
+
+ScmReader *
+scm_reader_construct_from_string(const char *string)
+{
+  ScmStringReader *reader;
+
+  assert(string != NULL);
+
+  reader = scm_memory_allocate(sizeof(ScmStringReader));
+  scm_initialize_base(&reader->base,
+                      read_line_from_string,
+                      read_char_from_string,
+                      is_string_eof);
+  reader->string = reader->ptr = string;
+  reader->len = strlen(string);
+
+  return (ScmReader *)reader;
 }
 
 int
