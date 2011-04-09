312cc65fabd244853b3570946b25b85de430f952
Hiroki MOTOYAMA <mtymhrk@gmail.com>
2011-04-10 02:47:12 +0900

    add test cases
    --HG--
    branch : implement_vm

--- test/test_iseq.c
+++ test/test_iseq.c
@@ -185,6 +185,122 @@ test_scm_iseq_set_immv_get_immv_4(void)
 }
 
 void
+test_scm_iseq_set_primv_get_primv_1(void)
+{
+ ScmObj iseq = SCM_OBJ_INIT;
+
+  SCM_STACK_FRAME_PUSH(&iseq);
+
+  /* preprocess */
+  SCM_SETQ(iseq, scm_iseq_new(SCM_MEM_ALLOC_HEAP));
+
+  /* action */
+  scm_inst_t inst;
+  inst.primv.op = SCM_OPCODE_PUSH_PRIMVAL;
+  inst.primv.primval = SCM_INST_PRIMVAL_MAX;
+
+  scm_iword_t *next_write =
+    scm_iseq_set_word(iseq, SCM_ISEQ_SEQ(iseq), inst.iword);
+  scm_inst_t actual;
+  scm_iword_t *next_read = scm_iseq_get_word(SCM_ISEQ_SEQ(iseq), &actual.iword);
+
+  /* postcondition check */
+  cut_assert_not_null(next_write);
+  cut_assert_equal_int(1, next_write - SCM_ISEQ_SEQ(iseq));
+  cut_assert_equal_uint(1, SCM_ISEQ_SEQ_LENGTH(iseq));
+  cut_assert_equal_uint(SCM_OPCODE_PUSH_PRIMVAL, actual.primv.op);
+  cut_assert_equal_int(SCM_INST_PRIMVAL_MAX, actual.primv.primval);
+  cut_assert_equal_pointer(next_read, next_write);
+}
+
+void
+test_scm_iseq_set_primv_get_primv_2(void)
+{
+ ScmObj iseq = SCM_OBJ_INIT;
+
+  SCM_STACK_FRAME_PUSH(&iseq);
+
+  /* preprocess */
+  SCM_SETQ(iseq, scm_iseq_new(SCM_MEM_ALLOC_HEAP));
+
+  /* action */
+  scm_inst_t inst;
+  inst.primv.op = SCM_OPCODE_PUSH_PRIMVAL;
+  inst.primv.primval  = 0;
+
+  scm_iword_t *next_write =
+    scm_iseq_set_word(iseq, SCM_ISEQ_SEQ(iseq), inst.iword);
+  scm_inst_t actual;
+  scm_iword_t *next_read = scm_iseq_get_word(SCM_ISEQ_SEQ(iseq), &actual.iword);
+
+  /* postcondition check */
+  cut_assert_not_null(next_write);
+  cut_assert_equal_int(1, next_write - SCM_ISEQ_SEQ(iseq));
+  cut_assert_equal_uint(1, SCM_ISEQ_SEQ_LENGTH(iseq));
+  cut_assert_equal_uint(SCM_OPCODE_PUSH_PRIMVAL, actual.primv.op);
+  cut_assert_equal_int(0, actual.primv.primval);
+  cut_assert_equal_pointer(next_read, next_write);
+}
+
+void
+test_scm_iseq_set_primv_get_primv_3(void)
+{
+ ScmObj iseq = SCM_OBJ_INIT;
+
+  SCM_STACK_FRAME_PUSH(&iseq);
+
+  /* preprocess */
+  SCM_SETQ(iseq, scm_iseq_new(SCM_MEM_ALLOC_HEAP));
+
+  /* action */
+  scm_inst_t inst;
+  inst.primv.op = SCM_OPCODE_PUSH_PRIMVAL;
+  inst.primv.primval  = -1;
+
+  scm_iword_t *next_write =
+    scm_iseq_set_word(iseq, SCM_ISEQ_SEQ(iseq), inst.iword);
+  scm_inst_t actual;
+  scm_iword_t *next_read = scm_iseq_get_word(SCM_ISEQ_SEQ(iseq), &actual.iword);
+
+  /* postcondition check */
+  cut_assert_not_null(next_write);
+  cut_assert_equal_int(1, next_write - SCM_ISEQ_SEQ(iseq));
+  cut_assert_equal_uint(1, SCM_ISEQ_SEQ_LENGTH(iseq));
+  cut_assert_equal_uint(SCM_OPCODE_PUSH_PRIMVAL, actual.primv.op);
+  cut_assert_equal_int(-1, actual.primv.primval);
+  cut_assert_equal_pointer(next_read, next_write);
+}
+
+void
+test_scm_iseq_set_primv_get_primv_4(void)
+{
+ ScmObj iseq = SCM_OBJ_INIT;
+
+  SCM_STACK_FRAME_PUSH(&iseq);
+
+  /* preprocess */
+  SCM_SETQ(iseq, scm_iseq_new(SCM_MEM_ALLOC_HEAP));
+
+  /* action */
+  scm_inst_t inst;
+  inst.primv.op = SCM_OPCODE_PUSH_PRIMVAL;
+  inst.primv.primval  = SCM_INST_PRIMVAL_MIN;
+
+  scm_iword_t *next_write =
+    scm_iseq_set_word(iseq, SCM_ISEQ_SEQ(iseq), inst.iword);
+  scm_inst_t actual;
+  scm_iword_t *next_read = scm_iseq_get_word(SCM_ISEQ_SEQ(iseq), &actual.iword);
+
+  /* postcondition check */
+  cut_assert_not_null(next_write);
+  cut_assert_equal_int(1, next_write - SCM_ISEQ_SEQ(iseq));
+  cut_assert_equal_uint(1, SCM_ISEQ_SEQ_LENGTH(iseq));
+  cut_assert_equal_uint(SCM_OPCODE_PUSH_PRIMVAL, actual.primv.op);
+  cut_assert_equal_int(SCM_INST_PRIMVAL_MIN, actual.primv.primval);
+  cut_assert_equal_pointer(next_read, next_write);
+}
+
+void
 test_scm_iseq__size_of_scm_inst_t_should_be_equal_to_scm_iword_t(void)
 {
   cut_assert_equal_uint(sizeof(scm_iword_t), sizeof(scm_inst_t));
