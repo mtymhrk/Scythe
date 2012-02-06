847a815c008d2aaf0d9deaef182c75d216989b99
Hiroki MOTOYAMA <mtymhrk@gmail.com>
2012-02-06 12:13:33 +0900

    scm_api_string_to_symbol を inline 関数に変更

--- api.c
+++ api.c
@@ -5,22 +5,3 @@
 #include "api.h"
 
 
-ScmObj
-scm_api_string_to_symbol(ScmObj str) /* GC OK */
-{
-  ScmObj sym = SCM_OBJ_NULL;
-
-  SCM_STACK_FRAME_PUSH(&str, &sym);
-
-  if (SCM_OBJ_IS_TYPE(str, &SCM_STRING_TYPE_INFO))
-    goto err;
-
-  return scm_symtbl_symbol(scm_vm_current_symtbl(), str);
-
- err:
-    /* TODO: ランタイムエラーをどう処理するか。
-     * 案: VM にエラー状態を保持し、戻り値では undefined オブジェクトのような
-     * ものを返す。
-     */
-  return SCM_OBJ_NULL; /* 仮実装 */
-}
--- api.h
+++ api.h
@@ -7,12 +7,21 @@
 #include "symbol.h"
 #include "pair.h"
 
+
+/** predicate ******************************************************/
+
 static inline ScmObj
 scm_api_eq_p(ScmObj obj1, ScmObj obj2)
 {
   return (SCM_OBJ_IS_SAME_INSTANCE(obj1, obj2) ?
           scm_vm_bool_true_instance() : scm_vm_bool_false_instance());
 }
+
+ScmObj scm_api_eqv_p(ScmObj obj1, ScmObj obj2); /* TODO: write me */
+ScmObj scm_api_equal_p(ScmObj obj1, ScmObj obj2); /* TODO: write me */
+
+
+/** Symbol *********************************************************/
 
 static inline ScmObj
 scm_api_symbol_to_string(ScmObj sym)
@@ -24,8 +33,17 @@ scm_api_symbol_to_string(ScmObj sym)
   return SCM_SYMBOL_STR(sym);
 }
 
-ScmObj scm_api_string_to_symbol(ScmObj str);
+static inline ScmObj
+scm_api_string_to_symbol(ScmObj str)
+{
+  if (SCM_OBJ_IS_TYPE(str, &SCM_STRING_TYPE_INFO))
+    return SCM_OBJ_NULL;         /* 仮実装 */
 
+  return scm_symtbl_symbol(scm_vm_current_symtbl(), str);
+}
+
+
+/** List and Pair  *************************************************/
 
 static inline ScmObj
 scm_api_cons(ScmObj car, ScmObj cdr)
@@ -53,5 +71,7 @@ scm_api_cdr(ScmObj pair)
 
   return scm_pair_cdr(pair);
 }
+
+
 
 #endif /* INCLUDE_API_H__ */
