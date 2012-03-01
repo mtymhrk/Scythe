7488c282d983cdb756afc79ec02d1a6121dee246
Hiroki MOTOYAMA <mtymhrk@gmail.com>
2012-03-01 16:51:42 +0900

    + subroutine のエラー処理をすこし変更 + scm_subrutine_gc_initialize を不要なので削除

--- procedure.c
+++ procedure.c
@@ -8,13 +8,13 @@
 ScmTypeInfo SCM_SUBRUTINE_TYPE_INFO = {
   NULL,                         /* pp_func              */
   sizeof(ScmSubrutine),         /* obj_size             */
-  scm_subrutine_gc_initialize,  /* gc_ini_func          */
+  NULL,                         /* gc_ini_func          */
   NULL,                         /* gc_fin_func          */
   NULL,                         /* gc_accept_func       */
   NULL,                         /* gc_accpet_func_weak  */
 };
 
-void
+int
 scm_subrutine_initialize(ScmObj subr, ScmSubrFunc func)
 {
   SCM_STACK_FRAME_PUSH(&subr);
@@ -23,6 +23,8 @@ scm_subrutine_initialize(ScmObj subr, ScmSubrFunc func)
   scm_assert(func != NULL);
 
   SCM_SUBRUTINE(subr)->subr_func = func;
+
+  return 0;
 }
 
 ScmObj
@@ -35,8 +37,10 @@ scm_subrutine_new(SCM_MEM_TYPE_T mtype, ScmSubrFunc func)
   scm_assert(func != NULL);
 
   subr = scm_capi_mem_alloc(&SCM_SUBRUTINE_TYPE_INFO, mtype);
+  if (scm_obj_null_p(subr)) return SCM_OBJ_NULL; /* [ERR]: [through] */
 
-  scm_subrutine_initialize(subr, func);
+  if (scm_subrutine_initialize(subr, func))
+    return SCM_OBJ_NULL;        /* [ERR]: [through] */
 
   return subr;
 }
@@ -44,24 +48,7 @@ scm_subrutine_new(SCM_MEM_TYPE_T mtype, ScmSubrFunc func)
 ScmObj
 scm_subrutine_call(ScmObj subr)
 {
-  ScmObj ret = SCM_OBJ_INIT;
-
-  SCM_STACK_FRAME_PUSH(&ret);
-
   scm_assert_obj_type(subr, &SCM_SUBRUTINE_TYPE_INFO);
 
-  ret = SCM_SUBRUTINE(subr)->subr_func();
-  if (scm_obj_null_p(ret))
-    ;                           /* TODO: error handling */
-
-  return ret;
-}
-
-void
-scm_subrutine_gc_initialize(ScmObj obj, ScmObj mem)
-{
-  scm_assert_obj_type(obj, &SCM_SUBRUTINE_TYPE_INFO);
-  scm_assert(scm_obj_not_null_p(mem));
-
-  SCM_SUBRUTINE(obj)->subr_func = NULL;
+  return SCM_SUBRUTINE(subr)->subr_func();
 }
--- procedure.h
+++ procedure.h
@@ -15,11 +15,9 @@ struct ScmSubrutineRec {
   ScmSubrFunc subr_func;
 };
 
-void scm_subrutine_initialize(ScmObj subr, ScmSubrFunc func);
+int scm_subrutine_initialize(ScmObj subr, ScmSubrFunc func);
 ScmObj scm_subrutine_new(SCM_MEM_TYPE_T mtype, ScmSubrFunc func);
 ScmObj scm_subrutine_call(ScmObj subr);
-
-void scm_subrutine_gc_initialize(ScmObj obj, ScmObj mem);
 
 
 #endif /* INCLUDE_PROCEDURE_H__ */
