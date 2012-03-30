5f2affa28c6fe651d5dc7c32921380caf93d3894
Hiroki MOTOYAMA <mtymhrk@gmail.com>
2012-03-30 14:48:33 +0900

    ScmException を追加

---
+++ exception.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "object.h"
+#include "api.h"
+#include "exception.h"
+
+ScmTypeInfo SCM_EXCEPTION_TYPE_INFO = {
+  .pp_func = scm_exception_pretty_print,
+  .obj_size = sizeof(ScmException),
+  .gc_ini_func = scm_exception_gc_initialize,
+  .gc_fin_func = scm_exception_gc_fianlize,
+  .gc_accept_func = scm_exception_gc_accept,
+  .gc_accept_func_weak = NULL
+};
+
+int
+scm_exception_initialize(ScmObj exc, ScmObj msg, size_t n, va_list irris)
+{
+  ScmObj ir = SCM_OBJ_INIT;
+
+  scm_assert_obj_type(exc, &SCM_EXCEPTION_TYPE_INFO);
+  scm_assert(scm_capi_string_p(msg));
+  scm_assert(n <= SCM_EXCEPTION_IRRITANTS_MAX);
+
+  SCM_EXCEPTION(exc)->irritants = scm_capi_malloc(sizeof(ScmObj) * n);
+  if (SCM_EXCEPTION(exc)->irritants == NULL)
+    return -1;                  /* [ERR]: [through] */
+
+  SCM_SLOT_SETQ(ScmException, exc, msg, msg);
+
+  for (size_t i = 0; i < n; i++) {
+    ir = va_arg(irris, ScmObj);
+    SCM_SLOT_SETQ(ScmException, exc, irritants[i], ir);
+  }
+
+  return 0;
+}
+
+void
+scm_exception_finalize(ScmObj exc)
+{
+  scm_assert_obj_type(exc, &SCM_EXCEPTION_TYPE_INFO);
+
+  scm_capi_free(SCM_EXCEPTION(exc)->irritants);
+  SCM_EXCEPTION(exc)->irritants = NULL;
+  SCM_EXCEPTION(exc)->nr_irris = 0;
+  SCM_EXCEPTION(exc)->msg = SCM_OBJ_NULL;
+}
+
+ScmObj
+scm_exception_new(SCM_MEM_TYPE_T mtype, ScmObj msg, size_t n, ...)
+{
+  ScmObj exc = SCM_OBJ_INIT;
+  va_list irris;
+  int rslt;
+
+  scm_assert(scm_capi_string_p(msg));
+  scm_assert(n <= SCM_EXCEPTION_IRRITANTS_MAX);
+
+  exc = scm_capi_mem_alloc(&SCM_EXCEPTION_TYPE_INFO, mtype);
+  if (scm_obj_null_p(exc)) return SCM_OBJ_NULL; /* [ERR]: [through] */
+
+  va_start(irris, n);
+  rslt = scm_exception_initialize(exc, msg, n, irris);
+  va_end(irris);
+
+  if (rslt < 0) return SCM_OBJ_NULL; /* [ERR]: [through] */
+
+  return exc;
+}
+
+int
+scm_exception_pretty_print(ScmObj obj, ScmObj port, bool write_p)
+{
+  ScmObj ro;
+  int rslt;
+
+  scm_assert_obj_type(obj, &SCM_EXCEPTION_TYPE_INFO);
+
+  if (write_p) {
+    char cstr[32];
+
+    snprintf(cstr, sizeof(cstr), " 0x%llx>", (unsigned long long)obj);
+
+    rslt = scm_capi_write_cstr("#<exception ", SCM_ENC_ASCII, port);
+    if (rslt < 0) return -1;    /* [ERR]: [through] */
+
+    ro = scm_api_write_string(SCM_EXCEPTION(obj)->msg, port);
+    if (scm_obj_null_p(ro)) return -1; /* [ERR]: [through] */
+
+    rslt = scm_capi_write_cstr(cstr, SCM_ENC_ASCII, port);
+    if (rslt < 0) return -1;    /* [ERR]: [through] */
+  }
+  else {
+    rslt = scm_capi_write_cstr("Exception: ", SCM_ENC_ASCII, port);
+    if (rslt < 0) return -1;    /* [ERR]: [through] */
+
+    if (scm_obj_not_null_p(SCM_EXCEPTION(obj)->msg)) {
+      const char *dlm;
+
+      ro = scm_api_write_string(SCM_EXCEPTION(obj)->msg, port);
+      if (scm_obj_null_p(ro)) return -1; /* [ERR]: [through] */
+
+      dlm = ": ";
+      for (size_t i = 0; i < SCM_EXCEPTION(obj)->nr_irris; i++) {
+        rslt = scm_capi_write_cstr(dlm, SCM_ENC_ASCII, port);
+        if (rslt < 0) return -1;    /* [ERR]: [through] */
+
+        ro = scm_api_write(SCM_EXCEPTION(obj)->irritants[i], port);
+        if (scm_obj_null_p(ro)) return -1; /* [ERR]: [through] */
+
+        dlm = ", ";
+      }
+    }
+  }
+
+  return 0;
+}
+
+void
+scm_exception_gc_initialize(ScmObj obj, ScmObj mem)
+{
+  scm_assert_obj_type(obj, &SCM_EXCEPTION_TYPE_INFO);
+  scm_assert(scm_obj_not_null_p(mem));
+
+  SCM_EXCEPTION(obj)->msg = SCM_OBJ_NULL;
+  SCM_EXCEPTION(obj)->irritants = NULL;
+  SCM_EXCEPTION(obj)->nr_irris = 0;
+}
+
+void
+scm_exception_gc_fianlize(ScmObj obj)
+{
+  scm_exception_finalize(obj);
+}
+
+int
+scm_exception_gc_accept(ScmObj obj, ScmObj mem, ScmGCRefHandlerFunc handler)
+{
+  int rslt = SCM_GC_REF_HANDLER_VAL_INIT;
+
+  scm_assert_obj_type(obj, &SCM_EXCEPTION_TYPE_INFO);
+  scm_assert(scm_obj_not_null_p(mem));
+  scm_assert(handler != NULL);
+
+  rslt = SCM_GC_CALL_REF_HANDLER(handler, obj, SCM_EXCEPTION(obj)->msg, mem);
+  if (scm_gc_ref_handler_failure_p(rslt)) return rslt;
+
+  for (size_t i = 0; i < SCM_EXCEPTION(obj)->nr_irris; i++) {
+    rslt = SCM_GC_CALL_REF_HANDLER(handler, obj,
+                                   SCM_EXCEPTION(obj)->irritants[i], mem);
+    if (scm_gc_ref_handler_failure_p(rslt)) return rslt;
+  }
+
+  return rslt;
+}
---
+++ exception.h
@@ -0,0 +1,50 @@
+#ifndef INCLUDE_EXCEPTION_H__
+#define INCLUDE_EXCEPTION_H__
+
+#include <stddef.h>
+#include <limits.h>
+
+typedef struct ScmExceptionRec ScmException;
+
+#define SCM_EXCEPTION(obj) ((ScmException *)(obj))
+
+#include "object.h"
+
+struct ScmExceptionRec {
+  ScmObjHeader header;
+  ScmObj msg;
+  ScmObj *irritants;
+  size_t nr_irris;
+};
+
+extern ScmTypeInfo SCM_EXCEPTION_TYPE_INFO;
+
+#define SCM_EXCEPTION_IRRITANTS_MAX (SIZE_MAX / sizeof(ScmObj))
+
+int scm_exception_initialize(ScmObj exc, ScmObj msg, size_t n, va_list irris);
+void scm_exception_finalize(ScmObj exc);
+ScmObj scm_exception_new(SCM_MEM_TYPE_T mtype, ScmObj msg, size_t n, ...);
+int scm_exception_pretty_print(ScmObj obj, ScmObj port, bool write_p);
+void scm_exception_gc_initialize(ScmObj obj, ScmObj mem);
+void scm_exception_gc_fianlize(ScmObj obj);
+int scm_exception_gc_accept(ScmObj obj, ScmObj mem,
+                            ScmGCRefHandlerFunc handler);
+
+inline size_t
+scm_exception_nr_irritants(ScmObj exc)
+{
+  scm_assert_obj_type(exc, &SCM_EXCEPTION_TYPE_INFO);
+
+  return SCM_EXCEPTION(exc)->nr_irris;
+}
+
+inline ScmObj
+scm_exception_irritant(ScmObj exc, size_t idx)
+{
+  scm_assert_obj_type(exc, &SCM_EXCEPTION_TYPE_INFO);
+  scm_assert(idx < SCM_EXCEPTION(exc)->nr_irris);
+
+  return SCM_EXCEPTION(exc)->irritants[idx];
+}
+
+#endif /* INCLUDE_EXCEPTION_H__ */
