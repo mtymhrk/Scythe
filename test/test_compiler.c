b267ede0636692c856bf802f3c422ce37a22b195
Hiroki MOTOYAMA <mtymhrk@gmail.com>
2012-07-13 14:43:24 +0900

    compiler の unit test の test case を追加

--- test/test_compiler.c
+++ test/test_compiler.c
@@ -452,6 +452,40 @@ test_scm_api_compile__refer_free_variable_1(void)
 }
 
 void
+test_scm_api_compile__refer_free_variable_2(void)
+{
+  ScmObj exp = SCM_OBJ_INIT, port = SCM_OBJ_INIT;
+  ScmObj actual = SCM_OBJ_INIT, expected = SCM_OBJ_INIT;
+  const char *exp_str = "(lambda (f1 f2) (lambda (b1 b2) (set! f2 'a) f2))";
+  const char *asm_str = "((asm-close 0"
+                        "   ((box 1)"
+                        "    (sref 1)"
+                        "    (push)"
+                        "    (asm-close 1"
+                        "      ((immval a)(cset 0)"
+                        "       (cref 0)(unbox)"
+                        "       (return 2)))"
+                        "    (return 2)))"
+                        " (return 0))";
+
+  SCM_STACK_FRAME_PUSH(&exp, &port,
+                       &actual, &expected);
+
+  port = scm_capi_open_input_string_from_cstr(exp_str, SCM_ENC_ASCII);
+  exp = scm_api_read(port);
+
+  port = scm_capi_open_input_string_from_cstr(asm_str, SCM_ENC_ASCII);
+  expected = scm_api_read(port);
+
+  actual = scm_api_compile(exp);
+
+  /* scm_api_write(exp, SCM_OBJ_NULL); scm_api_newline(SCM_OBJ_NULL); */
+  /* scm_api_write(actual, SCM_OBJ_NULL); scm_api_newline(SCM_OBJ_NULL); */
+
+  cut_assert_true(scm_capi_true_p(scm_api_equal_P(expected, actual)));
+}
+
+void
 test_scm_api_compile__set_free_variable_1(void)
 {
   ScmObj exp = SCM_OBJ_INIT, port = SCM_OBJ_INIT;
@@ -477,8 +511,8 @@ test_scm_api_compile__set_free_variable_1(void)
 
   actual = scm_api_compile(exp);
 
-  scm_api_write(exp, SCM_OBJ_NULL); scm_api_newline(SCM_OBJ_NULL);
-  scm_api_write(actual, SCM_OBJ_NULL); scm_api_newline(SCM_OBJ_NULL);
+  /* scm_api_write(exp, SCM_OBJ_NULL); scm_api_newline(SCM_OBJ_NULL); */
+  /* scm_api_write(actual, SCM_OBJ_NULL); scm_api_newline(SCM_OBJ_NULL); */
 
   cut_assert_true(scm_capi_true_p(scm_api_equal_P(expected, actual)));
 }
