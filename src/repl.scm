07fffa06ed240dc2fa0261073176c9b6e5ba77dc
Hiroki MOTOYAMA <mtymhrk@gmail.com>
2014-06-06 00:13:06 +0900

    with-module ではなく select-module を使うよう read-eval-print-loop を変更

--- src/repl.scm
+++ src/repl.scm
@@ -1,17 +1,18 @@
 
 
-(with-module (scythe internal repl)
-  (define (read-eval-print-loop)
-    (when (call/cc
-           (lambda (k)
-             (with-exception-handler
-              (lambda (e) (display e) (newline) (k #t))
-              (lambda ()
+(select-module (scythe internal repl))
+
+(define (read-eval-print-loop)
+  (when (call/cc
+         (lambda (k)
+           (with-exception-handler
+            (lambda (e) (display e) (newline) (k #t))
+            (lambda ()
+              (display "> ") (flush-output-port)
+              (let rec ((exp (read)))
+                (when (eof-object? exp) (k #f))
+                (write (eval exp))
+                (newline)
                 (display "> ") (flush-output-port)
-                (let rec ((exp (read)))
-                  (when (eof-object? exp) (k #f))
-                  (write (eval exp))
-                  (newline)
-                  (display "> ") (flush-output-port)
-                  (rec (read)))))))
-      (read-eval-print-loop))))
+                (rec (read)))))))
+    (read-eval-print-loop)))
