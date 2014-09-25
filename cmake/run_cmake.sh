19df0b6ee91cf17d81d6f6d1cb2abeda5bad6246
Hiroki MOTOYAMA <mtymhrk@gmail.com>
2014-09-26 00:00:46 +0900

    cmake によるビルドツリー作成後に実行していたスクリプトの実行の仕方を変更
    スクリプトを cmake/hooks 配下に設置するようにし、run_cmake.sh 内の
    post_cmake_hooks 変数に設定してあるスクリプトを実行するように変更。
    
    run_cmake.sh は設定ファイル cmake/run_cmake.conf を読み込むようにし、
    post_cmake_hooks 変数値を設定ファイルで指定できるように変更。
    cmake/run_cmake.conf は Mercurial の無視対象として指定。
    
    --HG--
    rename : cmake/gen_flymake.rb => cmake/hooks/gen_flymake.rb

--- cmake/run_cmake.sh
+++ cmake/run_cmake.sh
@@ -1,22 +1,35 @@
 #!/bin/sh
 
 PRJ_DIR=`pwd`
+TOOLS_DIR=${PRJ_DIR}/cmake
+CONF_FILE=${TOOLS_DIR}/run_cmake.conf
 
 if [ ! -n "${1}" ]; then
     echo "${0}: too few arguments"
     echo "Usage: ${0} BUILD_DIR_NAME"
     exit 1
 else
-    BUILD_NAME="${1}"
+  BUILD_DIR="${1}"
 fi
 
-if [ ! -e "$BUILD_NAME" ]; then
-  echo "${0}: MAKE DIR ${BUILD_NAME}"
-  mkdir $BUILD_NAME
+post_cmake_hooks=""
+
+if [ -f "${CONF_FILE}" ]; then
+  echo "${0}: LOAD ${CONF_FILE}"
+  . ${CONF_FILE}
+fi
+
+if [ ! -e "${BUILD_DIR}" ]; then
+  echo "${0}: MAKE DIR ${BUILD_DIR}"
+  mkdir ${BUILD_DIR}
 fi
 
 echo "${0}: EXECUTE cmake"
-(cd $BUILD_NAME; cmake $PRJ_DIR)
+(cd ${BUILD_DIR}; cmake ${PRJ_DIR})
 
-echo "${0}: EXECUTE gen_flymake.rb"
-ruby cmake/gen_flymake.rb $BUILD_NAME
+for hook in ${post_cmake_hooks}; do
+  if [ -x "${TOOLS_DIR}/hooks/${hook}" ]; then
+    echo "${0}: EXECUTE ${hook}"
+    ${TOOLS_DIR}/hooks/${hook} ${BUILD_DIR}
+  fi
+done
